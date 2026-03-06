1ffe0ae49a6badf0254ff7f753d601b74f49465e
wymbt <matheushenrikgomesdossantos@gmail.com>
2026-03-06 15:03:16 -0400

    main

--- CONFIG/JS/COPY.js
+++ CONFIG/JS/COPY.js
@@ -1,11 +1,24 @@
 function copyCode() {
-    fetch("CONFIG/ASSETS/CODES/GAUGELINEAR.TXT")
-        .then(response => response.text())
-        .then(text => {
-            navigator.clipboard.writeText(text);
-            alert("Código copiado!");
-        })
-        .catch(error => {
-            console.error("Erro:", error);
-        });
+
+fetch("CONFIG/ASSETS/CODES/GAUGELINEAR.TXT")
+
+.then(response => response.text())
+
+.then(text => {
+
+navigator.clipboard.writeText(text)
+
+.then(() => {
+alert("Código copiado!");
+});
+
+})
+
+.catch(error => {
+
+console.error("Erro:", error);
+alert("Não foi possível copiar o código");
+
+});
+
 }

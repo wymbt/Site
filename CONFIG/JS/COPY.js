bcb64c32d2163dc2cc11b368a10ed2b47599a7f7
wymbt <matheushenrikgomesdossantos@gmail.com>
2026-03-06 15:20:32 -0400

    MAIN

--- CONFIG/JS/COPY.js
+++ CONFIG/JS/COPY.js
@@ -22,3 +22,128 @@ alert("ERROR!");
 });
 
 }
+
+function copyCode2() {
+
+fetch("CONFIG/ASSETS/CODES/RANKINGLINEAR.TXT")
+
+.then(response => response.text())
+
+.then(text => {
+
+navigator.clipboard.writeText(text)
+
+.then(() => {
+alert("CODE COPIED!");
+});
+
+})
+
+.catch(error => {
+
+console.error("Erro:", error);
+alert("ERROR!");
+
+});
+
+}
+
+function copyCode3() {
+
+fetch("CONFIG/ASSETS/CODES/RANKINGLISTA.TXT")
+
+.then(response => response.text())
+
+.then(text => {
+
+navigator.clipboard.writeText(text)
+
+.then(() => {
+alert("CODE COPIED!");
+});
+
+})
+
+.catch(error => {
+
+console.error("Erro:", error);
+alert("ERROR!");
+
+});
+
+}
+
+function copyCode4() {
+
+fetch("CONFIG/ASSETS/CODES/BARRAS.TXT")
+
+.then(response => response.text())
+
+.then(text => {
+
+navigator.clipboard.writeText(text)
+
+.then(() => {
+alert("CODE COPIED!");
+});
+
+})
+
+.catch(error => {
+
+console.error("Erro:", error);
+alert("ERROR!");
+
+});
+
+}
+
+function copyCode5() {
+
+fetch("CONFIG/ASSETS/CODES/RANKINGBARRAS.TXT")
+
+.then(response => response.text())
+
+.then(text => {
+
+navigator.clipboard.writeText(text)
+
+.then(() => {
+alert("CODE COPIED!");
+});
+
+})
+
+.catch(error => {
+
+console.error("Erro:", error);
+alert("ERROR!");
+
+});
+
+}
+
+function copyCode6() {
+
+fetch("CONFIG/ASSETS/CODES/LINHAS.TXT")
+
+.then(response => response.text())
+
+.then(text => {
+
+navigator.clipboard.writeText(text)
+
+.then(() => {
+alert("CODE COPIED!");
+});
+
+})
+
+.catch(error => {
+
+console.error("Erro:", error);
+alert("ERROR!");
+
+});
+
+}
